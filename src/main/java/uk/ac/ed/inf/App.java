70d5971c7adbddcec57d5c614a0d1549b62d3e85
rossc2k3 <alloriginalnamesused@gmail.com>
2023-10-02 17:31:06 +0100

    progress on ordervalidator

---
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -0,0 +1,17 @@
+package uk.ac.ed.inf;
+
+import uk.ac.ed.inf.ilp.constant.OrderStatus;
+import uk.ac.ed.inf.ilp.data.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    OrderStatus orderstatus = OrderStatus.DELIVERED;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/uk/ac/ed/inf/OrderValidator.java
@@ -0,0 +1,42 @@
+package uk.ac.ed.inf;
+
+
+import uk.ac.ed.inf.ilp.data.Order;
+import uk.ac.ed.inf.ilp.data.Restaurant;
+import uk.ac.ed.inf.ilp.interfaces.OrderValidation;
+import uk.ac.ed.inf.ilp.constant.OrderValidationCode;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.YearMonth;
+
+
+public class OrderValidator implements OrderValidation
+{
+    @Override
+    public Order validateOrder(Order orderToValidate, Restaurant[] definedRestaurants)
+    {
+
+        if(!orderToValidate.getCreditCardInformation().getCreditCardNumber().matches("^[0-9]{16}$"))
+        {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.CARD_NUMBER_INVALID);
+        }
+        else if(orderToValidate.getCreditCardInformation().getCvv().matches("^[0-9]{3}$"))
+        {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.CVV_INVALID);
+        }
+        //change YearMonth.now to order date pls :3
+        else if(orderToValidate.getCreditCardInformation().getCreditCardExpiry().matches("(0[1-9]|1[0-2])/[0-9][0-9]") &&
+                YearMonth.parse(orderToValidate.getCreditCardInformation().getCreditCardExpiry(), DateTimeFormatter.ofPattern("MM/yy")).isBefore( YearMonth.parse(YearMonth.now().toString(), DateTimeFormatter.ofPattern("MM/yy")) ) )
+        {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.EXPIRY_DATE_INVALID);
+        }
+        else
+        {
+            orderToValidate.setOrderValidationCode(OrderValidationCode.NO_ERROR);
+        }
+        return orderToValidate;
+    }
+}
+
+
